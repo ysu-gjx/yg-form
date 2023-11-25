a50e64a1c8140af4d3e0f6e7823d38c663414c7b
gjx <252932110@qq.com>
2023-11-26 01:08:51 +0800

    complete NumberField

--- lib/fields/NumberField.tsx
+++ lib/fields/NumberField.tsx
@@ -1,11 +1,27 @@
 import { defineComponent } from 'vue'
+import { FieldPropsDefine } from '../types'
 
 export default defineComponent({
   name: 'NumberField',
-  props: {},
+  props: FieldPropsDefine,
   setup(props) {
     return () => {
-      return <div>this is NumberField</div>
+      const handleChange = (e: Event) => {
+        const target = e.target as HTMLInputElement
+        const num = Number(target.value)
+
+        if (Number.isNaN(num)) {
+          props.onChange(undefined)
+        } else {
+          props.onChange(num)
+        }
+      }
+
+      return (
+        <>
+          <input type="number" value={props.value} onInput={handleChange} />
+        </>
+      )
     }
   },
 })
